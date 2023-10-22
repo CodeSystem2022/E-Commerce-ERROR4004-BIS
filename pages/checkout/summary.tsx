df778ba784798da38f6b72cf823d047fb729e45f
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-22 15:46:46 -0300

    #feat/#6-resumne-de-la-orden -feat: completo la pagina de resumen del pedido

--- pages/checkout/summary.tsx
+++ pages/checkout/summary.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import NextLink from 'next/link'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material'
 
-const Summary = () => {
+import ShopLayout from '../../components/layouts/ShopLayout'
+import CartList from '../../components/cart/CartList'
+import OrderSummary from '../../components/cart/OrderSummary'
+
+const SummaryPage = () => {
   return (
-    <div>summary</div>
+    <ShopLayout
+      title='Orden summary'
+      pageDescription='Purchase summary'
+    >
+      <>
+        <Typography variant='h1' component='h1' my={ 4 }>
+          Orden summary
+        </Typography>
+        <Grid container spacing={ 2 }>
+          <Grid item xs={ 12 } sm={ 7 }>
+            <CartList />
+          </Grid>
+          <Grid item xs={ 12 } sm={ 5 }>
+            <Card className='summary-card'>
+              <CardContent>
+                <Typography variant='h2' mb={ 4 }>
+                  Summary (3 products)
+                </Typography>
+                <Divider sx={ { my: 1 } } />
+                <Box display='flex' justifyContent='space-between' my={ 1 }>
+                  <Typography variant='subtitle1' >
+                    Delivery address
+                  </Typography>
+                  <NextLink href='/checkout/address' passHref>
+                    <strong>Edit</strong>
+                  </NextLink>
+                </Box>
+                <Typography>María Eugenia Costa</Typography>
+                <Typography>123 calle de prueba</Typography>
+                <Typography>Buenos Aires, 1122</Typography>
+                <Typography>Argentina</Typography>
+                <Typography>+ 54 11 2222 3333</Typography>
+                <Divider sx={ { my: 1 } } />
+                <Box display='flex' justifyContent='end' my={ 1 }>
+                  <NextLink href='/cart' passHref>
+                    <strong>Edit</strong>
+                  </NextLink>
+                </Box>
+                <OrderSummary />
+                <Box sx={ { mt: 3 } }>
+                  <Button
+                    color='secondary'
+                    className='circular-btn'
+                    fullWidth
+                  >
+                    Confirm order
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </>
+    </ShopLayout>
   )
 }
 
-export default Summary
+export default SummaryPage
