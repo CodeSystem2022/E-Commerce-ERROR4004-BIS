4e915fb70b962e1703bab3aed366dd85cb253591
matias9486 <matias9486mdp@gmail.com>
2023-10-27 19:59:21 -0300

    feat:agregado modelo y schema usurios y carga de prueba

---
+++ interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface IUser{
+    _id     : string
+    name    : string
+    email   : string
+    password: string
+    role    : string
+
+    createdAt?: string
+    updateAt?: string
+}
