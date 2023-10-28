2c52b3fef2dc109393bd758b793a1cfdcb11a60e
matias9486 <matias9486mdp@gmail.com>
2023-10-28 00:26:06 -0300

    feat:funcion y endpoint para validar token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,3 +1,4 @@
+import { rejects } from 'assert'
 import jwt from 'jsonwebtoken'
 
 export const signToken = ( _id:string, email: string) => {
@@ -16,4 +17,25 @@ export const signToken = ( _id:string, email: string) => {
         //opciones
         { expiresIn: '30d' }
     )
+}
+
+export const isValidToken = ( token: string ):Promise<string> => {
+    if( !process.env.JWT_SECRET_SEED ) {
+        throw new Error('No hay semilla de JWT - Revisar variables de entorno')
+    }
+
+    return new Promise( (resolve, reject) => {
+
+        try{
+            jwt.verify( token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+                if( err ) return reject('JWT no es válido')
+
+                const { _id } = payload as { _id: string}
+
+                resolve(_id)
+            } )
+        } catch( error ) {
+            reject('JWT no es válido')
+        }
+    } )
 }
