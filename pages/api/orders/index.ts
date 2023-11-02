8646ca08ca9ada8a606135a21cb40b7bbbb2333c
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-02 09:44:35 -0300

    -chequeamos el precio con la base de datos y creamos un arreglo con nuesros productos

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,20 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { IOrder } from '../../../interfaces'
+import { getSession } from 'next-auth/react'
+import { db } from '../../../database'
+import { Product } from '../../../models'
 
 type Data = {
     message: string
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    switch( req.method) {
+    switch (req.method) {
         case 'POST':
-            return createOrder( req, res)
-        
+            return createOrder(req, res)
+
         default:
             return res.status(400).json({ message: 'Bad Request' })
-    }        
+    }
 }
 
-const createOrder = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
-    const body = req.body
-    return res.status(201).json( body )
+const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { orderItems, total } = req.body as IOrder
+
+    // Check there is a user
+    const session: any = await getSession({ req })
+    if (!session) { 
+        return res.status(401).json({ message: 'Need to be authenticated' })
+    }
+
+    // Check price of products with data base
+    const productsIds = orderItems.map(product => product._id)
+    await db.connect()
+    
+    const dbProducts = await Product.find({ _id: { $in: productsIds } })
+    
+    console.log('dbProducts ', dbProducts)
+
+    return res.status(201).json(req.body)
 }
