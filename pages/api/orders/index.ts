2cb42444b89bd240ac1c4df36d221eed432d57ae
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-02 09:59:32 -0300

    -corroboro los totales, si no coinciden mando error

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -23,29 +23,41 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     // Check there is a user
     const session: any = await getSession({ req })
-    if (!session) { 
+    if (!session) {
         return res.status(401).json({ message: 'Need to be authenticated' })
     }
 
     // Check price of products with data base
     const productsIds = orderItems.map(product => product._id)
     await db.connect()
-    
+
     const dbProducts = await Product.find({ _id: { $in: productsIds } })
-    
+
     try {
-        
-        const subTotal = orderItems.reduce((prev, current) => { 
+
+        const subTotal = orderItems.reduce((prev, current) => {
             const currentPrice = dbProducts.find(prod => prod._id === current._id)?.price
-            if (!currentPrice) { 
+            if (!currentPrice) {
                 throw new Error('Check the shopping card, the product is not there')
             }
 
             return (currentPrice * current.quantity) + prev
         }, 0)
 
-    } catch (error) {
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0)
+        const backendTotal = subTotal * (taxRate + 1)
+
+        if (total !== backendTotal) {
+            throw new Error("The total to paid it's wrong")
+        }
+
+        // Everithing it's fine
         
+
+    } catch (error: any) {
+        await db.disconnect()
+        console.log(error)
+        res.status(400).json({ message: error.message || 'Check server logs' })
     }
 
     return res.status(201).json(req.body)
