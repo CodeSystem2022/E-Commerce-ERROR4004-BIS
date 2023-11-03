50c45ee8315193b862802660b732f05e624545b6
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-03 19:14:53 -0300

    -agrego tabulaciones

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -96,6 +96,7 @@ const OrderPage: NextPage<OrderPagProps> = ({ order }) => {
                       (
                         <PayPalButtons
                           createOrder={ (data, actions) => {
+
                             return actions.order.create({
                               purchase_units: [
                                 {
@@ -104,14 +105,17 @@ const OrderPage: NextPage<OrderPagProps> = ({ order }) => {
                                   },
                                 },
                               ],
-                            });
+                            })
+
                           } }
+
                           onApprove={ (data, actions) => {
+
                             return actions.order!.capture().then((details) => {
                               console.log({ details })
-                              const name = details.payer.name.given_name;
-                              //alert(`Transaction completed by ${name}`);
-                            });
+                              const name = details.payer.name.given_name
+                            })
+
                           } }
                         />
                       )
