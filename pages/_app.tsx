585da9f0c82af8af344f9d231ef7c94d8c1ee2b3
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-03 19:05:10 -0300

    -corrijo comillas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { SWRConfig } from 'swr'
-
 import { lightTheme } from '../themes'
 import { AuthProvider, CartProvider, UiProvider } from '../context'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <PayPalScriptProvider options={{'clientId': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || ''}}>
-        
-      
-
+      <PayPalScriptProvider
+        options={ { 'clientId': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' } }
+      >
         <SWRConfig
           value={ {
             fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
