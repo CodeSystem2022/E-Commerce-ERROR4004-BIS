a56f2f8e3fd14aa719e6e4efce3fe4c7120b1807
matias9486 <matias9486mdp@gmail.com>
2023-11-03 18:05:41 -0300

    Generacion de token de verificacion

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -7,12 +8,52 @@ type Data = {
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
     switch (req.method) {
         case 'POST':
-            return payOrder(req, res)        
+            return payOrder(req, res)
+        
         default:
             return res.status(400).json({ message: 'Bad request' })
     }    
 }
 
-const payOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    return res.status(200).json({ message: 'Paid order' })
+const getPaypalBearerToken = async():Promise<string|null> => {
+    
+    const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
+    const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
+
+    const base64Token = Buffer.from(`${ PAYPAL_CLIENT }:${ PAYPAL_SECRET }`, 'utf-8').toString('base64');
+    const body = new URLSearchParams('grant_type=client_credentials');
+
+
+    try {
+        
+        const { data} = await axios.post( process.env.PAYPAL_OAUTH_URL || '', body, {
+            headers: {
+                'Authorization': `Basic ${ base64Token }`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        return data.access_token;
+
+
+    } catch (error) {
+        if ( axios.isAxiosError(error) ) {
+            console.log(error.response?.data);
+        } else {
+            console.log(error);
+        }
+
+        return null;
+    }
+}
+
+const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+        
+    const paypalBearerToken = await getPaypalBearerToken();
+
+    if ( !paypalBearerToken ) {
+        return res.status(400).json({ message: 'Could not confirm paypal token' })
+    }
+        
+    return res.status(200).json({ message: paypalBearerToken})
 }
