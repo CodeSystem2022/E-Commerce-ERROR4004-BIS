10d0ff53b60b45b33f5d0f167d3e69f568e05901
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-04 14:58:18 -0300

    -limpio la base de datos de ordenes

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { db, seedDatabase } from '../../database'
-import { Product, User } from '../../models'
+import { Order, Product, User } from '../../models'
 
 type Data = {
   message: string
@@ -23,6 +23,8 @@ export default async function handler(
 
   await Product.deleteMany()
   await Product.insertMany(seedDatabase.initialData.products)
+
+  await Order.deleteMany()
 
   await db.disconnect()
 
