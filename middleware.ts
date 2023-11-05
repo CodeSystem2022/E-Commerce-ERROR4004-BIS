be8857b81c674b1b127748b0e5c8ee2be041cddc
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-04 21:14:35 -0300

    -feat: completo el middleware para que si no es admin no vea el dashboard -feat: agrego mas stock de productos en la base

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,79 @@
+import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getToken } from 'next-auth/jwt'
 
 export async function middleware(req: NextRequest) {
 
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const requestedPage = req.nextUrl.pathname
+  const validRoles = ['admin', 'super-user', 'SEO']
 
   if (!session) {
-    const requestedPage = req.nextUrl.pathname
     const url = req.nextUrl.clone()
 
-    url.pathname = `/auth/login`
+    url.pathname = '/auth/login'
     url.search = `p=${ requestedPage }`
 
+    if (requestedPage.includes('/api')) {
+      return new Response(JSON.stringify({ message: 'No authorized' }), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    }
+
     return NextResponse.redirect(url)
+  }
+
+  if (requestedPage.includes('/api/admin') && !validRoles.includes(session.user.role)) {
+
+    return new Response(JSON.stringify({ message: 'No authorized' }), {
+      status: 401,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  if (requestedPage.includes('/admin') && !validRoles.includes(session.user.role)) {
+
+    return NextResponse.redirect(new URL('/', req.url))
   }
 
   return NextResponse.next()
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/checkout/address', '/checkout/summary']
+  matcher: [
+    '/checkout/:path*',
+    '/orders/:path*',
+    '/api/orders/:path*',
+    '/admin/:path*',
+    '/api/admin/:path*']
 }
+
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+// import { getToken } from 'next-auth/jwt'
+
+// export async function middleware(req: NextRequest) {
+
+//   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+//   if (!session) {
+//     const requestedPage = req.nextUrl.pathname
+//     const url = req.nextUrl.clone()
+
+//     url.pathname = `/auth/login`
+//     url.search = `p=${ requestedPage }`
+
+//     return NextResponse.redirect(url)
+//   }
+
+//   return NextResponse.next()
+// }
+
+// export const config = {
+//   matcher: ['/checkout/address', '/checkout/summary']
+// }
