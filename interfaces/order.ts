fce75649b0a2ea01d6689e6d6f0146b2e1995570
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-05 00:58:31 -0300

    -pagina de obtener todas las ordenes

--- interfaces/order.ts
+++ interfaces/order.ts
@@ -16,6 +16,8 @@ export interface IOrder {
     paidAt?: string
 
     transactionId?: string
+
+    createdAt: string
 }
 
 export interface IOrderItem {
--- pages/admin/orders.tsx
+++ pages/admin/orders.tsx
@@ -1,16 +1,124 @@
 import React from 'react'
 
-import AdminLayout from '../../components/layouts/AdminLayout'
+import useSWR from 'swr'
+
+import { Chip, Grid } from '@mui/material'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import { ConfirmationNumberOutlined } from '@mui/icons-material'
 
+import AdminLayout from '../../components/layouts/AdminLayout'
+import { IOrder, IUser } from '../../interfaces'
+
+const columns: GridColDef[] = [
+  {
+    field: 'id',
+    headerName: 'Order ID',
+    width: 250,
+    description: 'Order ID'
+  },
+  {
+    field: 'email',
+    headerName: 'E-mail',
+    width: 300,
+    description: 'Email'
+  },
+  {
+    field: 'name',
+    headerName: 'Full name',
+    width: 300,
+    description: 'Full name'
+  },
+  {
+    field: 'total',
+    headerName: 'Total',
+    width: 180,
+    description: "Order's total"
+  },
+  {
+    field: 'isPaid',
+    headerName: 'Paid',
+    width: 180,
+    description: "Order's total",
+    sortable: false,
+    renderCell: (params) => {
+      return (
+        params.row.isPaid ?
+          <Chip color='success' label='Paid' variant='outlined' />
+          :
+          <Chip color='error' label='Pending' variant='outlined' />
+      )
+    }
+  },
+  {
+    field: 'noProducts',
+    headerName: 'No.Products',
+    align: 'center',
+    width: 160,
+    description: "Order's total amount of products"
+  },
+  {
+    field: 'check',
+    headerName: 'See order',
+    width: 160,
+    description: "Order's total",
+    sortable: false,
+    renderCell: (params) => {
+      return (
+        <a href={ `/admin/orders/${ params.row.id }` } target="_blank" rel="noreferrer">
+          See order
+        </a>
+      )
+    }
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Created at',
+    width: 280,
+    description: "Order created at"
+  }
+
+]
+
 const OrdersPage = () => {
+
+  const { data, error } = useSWR<IOrder[]>('/api/admin/orders')
+
+  if (!data && !error) return <></>
+  console.log({ data }) // TODO: es paara debuguear el error, despues borrarlo
+
+  const rows = data!.map(order => ({
+    id: order._id,
+    // TODO: el user llega como null
+    // capaz que es lo que cambie del Object(user) a que sea user
+    // porque sino no buscaba bien el usuario por id
+    // por ahora lo dejo vacio, peor hay que arreglarlo
+    email: '', //(order.user as IUser).email,
+    name: '', //(order.user as IUser).name,
+    total: order.total,
+    isPaid: order.isPaid,
+    noProducts: order.numberOfItems,
+    createdAt: order.createdAt,
+  }))
+
   return (
     <AdminLayout
       title='Orders'
       subTitle='Maintenance of orders'
       icon={ <ConfirmationNumberOutlined /> }
     >
-      <h2>Orders</h2>
+      <Grid container className='fadeIn'>
+        <Grid
+          item
+          xs={ 12 }
+          sx={ { height: '650px', width: '100%' } }
+        >
+          <DataGrid
+            rows={ rows }
+            columns={ columns }
+            autoPageSize
+          />
+        </Grid>
+      </Grid>
     </AdminLayout>
   )
 }
