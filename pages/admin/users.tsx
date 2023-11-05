3aee37f42a79899ea562b1632b5df96974b926bf
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-04 22:45:06 -0300

    -se muestran los usuarios

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
-import AdminLayout from '../../components/layouts/AdminLayout'
+
+import { Grid } from '@mui/material'
 import { PeopleOutline } from '@mui/icons-material'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+
+import AdminLayout from '../../components/layouts/AdminLayout'
+import useSWR from 'swr'
+import { IUser } from '../../interfaces'
 
 const UsersPage = () => {
+
+  const { data, error } = useSWR<IUser>('/api/admin/users')
+
+  if (!data && !error) return <></>
+
+  const rows = data!.map(user => ({
+    id: user._id,
+    email: user.email,
+    name: user.name,
+    role: user.role
+  }))  
+
+  const columns: GridColDef[] = [
+    { field: 'email', headerName: 'E-Mail', width: 250 },
+    { field: 'name', headerName: 'Full name', width: 300 },
+    { field: 'role', headerName: 'role', width: 200 }
+  ]
+
   return (
     <AdminLayout
       title='Users'
       subTitle=''
-      icon={ <PeopleOutline />}
+      icon={ <PeopleOutline /> }
     >
-      <h1>hola</h1>
+      <Grid container className='fadeIn'>
+        <Grid
+          item
+          xs={ 12 }
+          sx={ { height: '650px', width: '100%' } }
+        >
+          <DataGrid
+            rows={ rows }
+            columns={ columns }
+            autoPageSize
+          />
+        </Grid>
+      </Grid>
     </AdminLayout>
   )
 }
