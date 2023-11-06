d39adea1ee585b9e75f3f57e867be8054389e29b
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-06 09:36:40 -0300

    -conectar los campos con el default value

--- pages/admin/products/[slug].tsx
+++ pages/admin/products/[slug].tsx
@@ -1,26 +1,52 @@
 import React, { FC } from 'react'
 import { GetServerSideProps } from 'next'
+import { useForm } from 'react-hook-form'
 
-import { DriveFileRenameOutline, SaveOutlined, UploadOutlined } from '@mui/icons-material'
 import { Box, Button, capitalize, Card, CardActions, CardMedia, Checkbox, Chip, Divider, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, ListItem, Paper, Radio, RadioGroup, TextField } from '@mui/material'
+import { DriveFileRenameOutline, SaveOutlined, UploadOutlined } from '@mui/icons-material'
 
+import AdminLayout from '../../../components/layouts/AdminLayout'
 import { IProduct } from '../../../interfaces'
 import { dbProducts } from '../../../database'
-import AdminLayout from '../../../components/layouts/AdminLayout'
 
 
 const validTypes = ['running', 'sneakers', 'soccer', 'basketball', 'driving', 'training']
 const validGender = ['men', 'women', 'kid', 'unisex']
 const validSizes = ['30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50']
 
+interface FormData {
+  _id?: string
+  description: string
+  images: string[]
+  inStock: number
+  price: number
+  sizes: string[]
+  slug: string
+  tags: string[]
+  title: string
+  type: string
+  gender: 'men' | 'women' | 'kid' | 'unisex'
+}
 interface ProductAdminPageProps {
   product: IProduct
 }
 
 const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({
+    defaultValues: product
+  })
+
   const onDeleteTag = (tag: string) => {
 
+  }
+
+  const onSubmit = (form: FormData) => {
+    console.log({ form })
   }
 
   return (
@@ -29,7 +55,7 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
       subTitle={ `Editing: ${ product.title }` }
       icon={ <DriveFileRenameOutline /> }
     >
-      <form>
+      <form onSubmit={ handleSubmit(onSubmit) }>
         <Box
           display='flex'
           justifyContent='end'
@@ -40,7 +66,7 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
             sx={ { width: '150px', my: 1 } }
             type="submit"
           >
-            Save
+            SAVE
           </Button>
         </Box>
 
@@ -53,12 +79,12 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               variant="filled"
               fullWidth
               sx={ { mb: 1 } }
-            // { ...register('name', {
-            //     required: 'Este campo es requerido',
-            //     minLength: { value: 2, message: 'Mínimo 2 caracteres' }
-            // })}
-            // error={ !!errors.name }
-            // helperText={ errors.name?.message }
+              { ...register('title', {
+                required: "This field title it's required",
+                minLength: { value: 2, message: 'At least 2 caracters' }
+              }) }
+              error={ !!errors.title }
+              helperText={ errors.title?.message }
             />
 
             <TextField
@@ -67,6 +93,12 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               fullWidth
               multiline
               sx={ { mb: 1 } }
+              { ...register('description', {
+                required: "This field description it's required",
+                minLength: { value: 3, message: 'At least 3 caracters' }
+              }) }
+              error={ !!errors.description }
+              helperText={ errors.description?.message }
             />
 
             <TextField
@@ -75,6 +107,12 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               variant="filled"
               fullWidth
               sx={ { mb: 1 } }
+              { ...register('inStock', {
+                required: "The field inventory it's required",
+                min: { value: 0, message: 'Minimum value: 0' }
+              }) }
+              error={ !!errors.inStock }
+              helperText={ errors.inStock?.message }
             />
 
             <TextField
@@ -83,6 +121,12 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               variant="filled"
               fullWidth
               sx={ { mb: 1 } }
+              { ...register('price', {
+                required: "The field price it's required",
+                min: { value: 0, message: 'Minimum value: 0' }
+              }) }
+              error={ !!errors.price }
+              helperText={ errors.price?.message }
             />
 
             <Divider sx={ { my: 1 } } />
@@ -127,12 +171,13 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               </RadioGroup>
             </FormControl>
 
-            <FormGroup sx={ {flexDirection: 'row'}}>
+            <FormGroup sx={ { flexDirection: 'row' } }>
               <FormLabel
                 sx={ {
                   marginRight: '12px',
                   display: 'flex',
-                  alignItems: 'center'
+                  alignItems: 'center',
+                  width: '100%'
                 } }
               >
                 Sizes
@@ -153,6 +198,12 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               variant="filled"
               fullWidth
               sx={ { mb: 1 } }
+              { ...register('slug', {
+                required: "The field slug it's required",
+                validate: (val) => val.trim().includes(' ') ? 'No blank spaces permited' : undefined
+              }) }
+              error={ !!errors.slug }
+              helperText={ errors.slug?.message }
             />
 
             <TextField
@@ -209,7 +260,7 @@ const ProductAdminPage: FC<ProductAdminPageProps> = ({ product }) => {
               <Grid container spacing={ 2 }>
                 {
                   product.images.map(img => (
-                    <Grid item xs={ 4 } sm={ 3 } key={ img }>
+                    <Grid item xs={ 4 } md={ 3 } key={ img }>
                       <Card>
                         <CardMedia
                           component='img'
